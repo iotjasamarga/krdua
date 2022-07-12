414772c5e406635ed4b9193f87ff75e991fdce46
AdityaOctoraPH <aditya.octora@gmail.com>
2022-07-12 16:53:18 +0700

    third_commit

--- database_python.py
+++ database_python.py
@@ -57,33 +57,53 @@ async def io_related():
             data_old = lines[0]
 
             cursor2= mydb1.cursor()
-            cursor2.execute("SELECT detect FROM kr2") #id_location data
+            cursor2.execute("SELECT id FROM kr2") #id_location data
             myid_location = cursor2.fetchall()
             id_location = str(myid_location[len(myid_location)-1][0])
-
+            logging.info(f"id location:{id_location}")
+            
             cursor3= mydb1.cursor()
             cursor3.execute("SELECT detect FROM kr2") #nonkr_details data
-            mynonkr_detail = cursor3.fetchall()
-            nonkr_details = str(mynonkr_detail[len(mynonkr_detail)-1][0])
-            logging.info(nonkr_details)
+            mynonkr_gambar = cursor3.fetchall()
+            nonkr_gambar = mynonkr_gambar[len(mynonkr_gambar)-1][0]
+            logging.info(f"gambar:{nonkr_gambar[0:100]}")
 
-            if os.stat(nonkr_details).st_size:
-                with open(nonkr_details, "rb") as img_file:
-                    b64_string = base64.b64encode(img_file.read())
-                    logging.info("converted")
+            cursor4= mydb1.cursor()
+            cursor4.execute("SELECT details FROM kr2") #nonkr_details data
+            mynonkr_detail = cursor4.fetchall()
+            nonkr_details = str(mynonkr_detail[len(mynonkr_detail)-1][0])
+            logging.info(f"detail:{nonkr_details[0:100]}")
+            
+            # if os.stat(nonkr_details).st_size:
+            #     with open(nonkr_details, "rb") as img_file:
+            #         b64_string = base64.b64encode(img_file.read())
+            #         logging.info("converted")
+
+            # else:
+            #     logging.error("no picture url")
+
+            if len(nonkr_gambar) > 0:
+                b64_string = base64.b64encode(nonkr_gambar)
+                logging.info("converted")
+                logging.info(b64_string[0:100])
+
+                f= open("data_base64_.txt","w+")
+                f.write(b64_string.decode())
+                f.close()
             else:
                 logging.error("no picture url")
 
             if data_new != data_old:
                 data_add = {
-                    "kode_lokasi": id_location,	
+                    "kode_lokasi": "1",	
                     "jenis_r2" : nonkr_details,
-                    "gambar": b64_string                            
+                    "gambar": b64_string.decode()                            
                 }
                 response = requests.post(url, headers=headers, json=data_add)
                         
                 if (response.json()['status'] == 1):                          
                     logging.info("status:" + str(response.json()['status']))
+                    logging.info("status_detail:" + str(response.json()))
                     f= open("data.txt","w+")
                     f.write(str(myid[len(myid)-1][0]))
                     f.close()
@@ -102,12 +122,12 @@ async def io_related():
         except mysql.connector.Error as err:
             logging.error(err.msg)    
 
-        await asyncio.sleep(1)
+        await asyncio.sleep(0.5)
 
 async def io_related2():
     while True:
         try:
-            socket.gethostbyaddr('192.168.43.25')
+            socket.gethostbyaddr('192.168.0.101')
             logging.info("device connected  ")
 
         except socket.herror:
